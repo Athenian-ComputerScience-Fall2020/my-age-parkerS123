2a7aa4518efb3faf9ef9619cccfe2e8f0ff543fa
parkerS123 <23pschwantes@athenian.org>
2020-09-09 21:07:15 +0000

    Finished for real age

--- my_code.py
+++ my_code.py
@@ -5,19 +5,19 @@
 
 def age_now(x):
     # My age now
-    print(f"I am currently {age} years old.\n")
+    print(f"I am currently {x} years old.\n")
 
 def age_1(y):
     # My age next year
-    print(f"Next year I'll be {age + 1} years old.\n")
+    print(f"Next year I'll be {y + 1} years old.\n")
 
 def age_10(z):
     # My age in 10 years
-    print(f"In 10 years, I'll be {age + 10}!\n")
+    print(f"In 10 years, I'll be {z + 10}!\n")
 
 def age_50(q):
     # My age in 50 years!
-    print(f"In 50 years, I'll be {age + 50}! Wow!\n")
+    print(f"In 50 years, I'll be {q + 50}! Wow!\n")
 
 
 
@@ -32,8 +32,9 @@ if __name__ == '__main__':
     #'''
     #after you have this working by defining "age" above, you can comment that line out and add a new line
     # below it to get the user's age as an input.
+
     age = input("Enter your age ")
-age = int(age)
+    age = int(age)
 
    # '''
    # a = input("Enter your age ")
@@ -50,7 +51,7 @@ age = int(age)
 
 
 
-age_now(age)   # run function age_now with argument age
-age_1(age)    # run function age_1 with argument age
-age_10(age)    # run function age_10 with argument age
-age_50(age)    # run function age_50 with argument age
+    age_now(age)   # run function age_now with argument age
+    age_1(age)    # run function age_1 with argument age
+    age_10(age)    # run function age_10 with argument age
+    age_50(age)    # run function age_50 with argument age
